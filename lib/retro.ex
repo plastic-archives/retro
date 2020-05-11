7068565b304b06af529c67dd53e8645ec187c7d6
c4710n <c4710n@users.noreply.github.com>
2020-05-11 15:23:36 +0800

    chore: cleanup Retro module

--- lib/retro.ex
+++ lib/retro.ex
@@ -2,17 +2,4 @@ defmodule Retro do
   @moduledoc """
   Documentation for `Retro`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Retro.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
