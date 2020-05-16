9f60955b2b56013509a7d0948b1526b7cde6e3ce
c4710n <c4710n@users.noreply.github.com>
2020-05-16 11:17:37 +0800

    chore: add aliases

--- mix.exs
+++ mix.exs
@@ -1,17 +1,19 @@
 defmodule Retro.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
   @github_url "https://github.com/c4710n/retro"
 
   def project do
     [
       app: :retro,
       description: "A toolkit for pragmatic programmers.",
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
+      aliases: aliases(),
 
       # Docs
       name: "Retro",
@@ -43,7 +45,17 @@ defmodule Retro.MixProject do
   defp package do
     [
       licenses: ["MIT"],
-      links: %{"GitHub" => @github_url}
+      links: %{GitHub: @github_url}
     ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", "#{@version}"])
+    System.cmd("git", ["push", "--tags"])
   end
 end
