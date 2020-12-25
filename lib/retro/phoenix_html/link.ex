58142fa37477e8e7849c4da39100a9869e1fd3d5
c4710n <c4710n@users.noreply.github.com>
2020-12-25 13:17:38 +0800

    feat: add nofollow support for external link

--- lib/retro/phoenix_html/link.ex
+++ lib/retro/phoenix_html/link.ex
@@ -8,20 +8,22 @@ defmodule Retro.Phoenix.HTML.Link do
 
   @doc """
   Generates a external link to the given URL with vulnerability care. Get more
-  details at [here](https://stackoverflow.com/a/15551842).
+  details by reading:
+  + [Open link in new tab or window](https://stackoverflow.com/a/15551842)
+  + [Prevent Reverse Tabnabbing Attacks With Proper noopener, noreferrer, and nofollow Attribution](https://web.archive.org/web/20201225051333/https://blog.bhanuteja.dev/noopener-noreferrer-and-nofollow-when-to-use-them-how-can-these-prevent-phishing-attacks?guid=none&deviceId=4b2457f9-8062-46ca-a224-b24488072b1c)
 
   This function is same as `Phoenix.HTML.Link.link/2` except that it adds two
   additional options:
 
       [
         target: "_blank",
-        rel: "noopener noreferrer"
+        rel: "noopener noreferrer nofollow"
       ]
 
   ## Examples
 
       iex> external_link("open page", to: "https://example.com") |> Phoenix.HTML.safe_to_string
-      "<a href=\\"https://example.com\\" rel=\\"noopener noreferrer\\" target=\\"_blank\\">open page</a>"
+      "<a href=\\"https://example.com\\" rel=\\"noopener noreferrer nofollow\\" target=\\"_blank\\">open page</a>"
 
   """
   @doc since: "0.2.0"
@@ -32,7 +34,7 @@ defmodule Retro.Phoenix.HTML.Link do
   def external_link(contents, opts) do
     default_opts = [
       target: "_blank",
-      rel: "noopener noreferrer"
+      rel: "noopener noreferrer nofollow"
     ]
 
     opts = Keyword.merge(default_opts, opts)
