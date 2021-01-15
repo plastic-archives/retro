405a92244f5d8c0b42bf52ec6d340c56e512ec9c
c4710n <c4710n@users.noreply.github.com>
2021-01-15 17:39:56 +0800

    release: bump version

--- lib/retro/phoenix_router.ex
+++
@@ -1,7 +0,0 @@
-defmodule Retro.Phoenix.Router do
-  @moduledoc """
-  Toolkit for extending `Phoenix.Router`.
-  """
-
-  @moduledoc since: "0.3.0"
-end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Retro.MixProject do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "1.0.0"
   @github_url "https://github.com/c4710n/retro"
 
   def project do
