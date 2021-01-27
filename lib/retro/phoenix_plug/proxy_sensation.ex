cc7dea75fda97caf7673649466b89782a5308350
c4710n <c4710n@users.noreply.github.com>
2021-01-27 14:28:42 +0800

    feat: resolve remote_ip in Retro.Phoenix.Plug.ProxySensation

--- lib/retro/phoenix_plug/proxy_sensation.ex
+++ lib/retro/phoenix_plug/proxy_sensation.ex
@@ -9,16 +9,26 @@ defmodule Retro.Phoenix.Plug.ProxySensation do
   * `x-forwarded-host`
   * `x-forwarded-server`
 
-  Devrive port from following headers:
+  Derive port from following headers:
   * `x-forwarded-port`
 
-  ## Examples
+  Derive remote_ip from following headers:
+  * `x-forwarded-for`
+  * `x-real-ip`
 
-      iex> base_url(%Plug.Conn{req_headers: [ {"x-forwarded-host", "example.com"}, {"x-forwarded-proto", "https"} ]})
-      "https://example.com"
 
-      iex> base_url(%Plug.Conn{})
-      nil
+  # Configuration
+
+  The :remote_ip is available as [logger metadata](https://hexdocs.pm/logger/master/Logger.html#module-metadata).
+  To see the IP address in your log output, configure your logger backends to
+  include the :remote_ip metadata:
+
+  ```elixir
+  config :logger, :console,
+    format: "$time $metadata[$level] $message\n",
+    metadata: [:request_id, :remote_ip]
+  ```
+
   """
   @moduledoc since: "1.0.0"
 
@@ -27,18 +37,24 @@ defmodule Retro.Phoenix.Plug.ProxySensation do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    req_headers =
+    headers =
       conn
       |> Map.get(:req_headers)
       |> Enum.into(%{})
 
-    scheme = Map.get(req_headers, "x-forwarded-proto")
+    conn
+    |> resolve_router_url(headers)
+    |> resolve_remote_ip(headers)
+  end
+
+  def resolve_router_url(conn, headers) do
+    scheme = Map.get(headers, "x-forwarded-proto")
 
     host =
-      Map.get(req_headers, "x-forwarded-host") ||
-        Map.get(req_headers, "x-forwarded-server")
+      Map.get(headers, "x-forwarded-host") ||
+        Map.get(headers, "x-forwarded-server")
 
-    port = Map.get(req_headers, "x-forwarded-port")
+    port = Map.get(headers, "x-forwarded-port")
     port = port && String.to_integer(port)
 
     if scheme && host do
@@ -50,5 +66,27 @@ defmodule Retro.Phoenix.Plug.ProxySensation do
     else
       conn
     end
+  end
+
+  def resolve_remote_ip(conn, headers) do
+    ip = Map.get(headers, "x-forwarded-for") || Map.get(headers, "x-real-ip")
+
+    Logger.metadata(remote_ip: ip)
+
+    case parse_ip(ip) do
+      {:error, _} ->
+        conn
+
+      {:ok, remote_ip} ->
+        %{conn | remote_ip: remote_ip}
+    end
+  end
+
+  defp parse_ip(nil) do
+    {:error, :empty}
+  end
+
+  defp parse_ip(ip) do
+    ip |> to_charlist |> :inet.parse_strict_address()
   end
 end
